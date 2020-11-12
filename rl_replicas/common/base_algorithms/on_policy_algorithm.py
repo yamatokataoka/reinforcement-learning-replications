904409634ebd2fe9a3ce42d920dc19144ff450e2
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-11-12 09:36:55 +0000

    Add logging for Episode Return STD

--- rl_replicas/common/base_algorithms/on_policy_algorithm.py
+++ rl_replicas/common/base_algorithms/on_policy_algorithm.py
@@ -122,6 +122,7 @@ class OnPolicyAlgorithm(ABC):
       logger.info('Total episodes:         {:<8.3g}'.format(self.current_total_episodes))
 
       logger.info('Average Episode Return: {:<8.3g}'.format(np.mean(episode_returns)))
+      logger.info('Episode Return STD:     {:<8.3g}'.format(np.std(episode_returns)))
       logger.info('Max Episode Return:     {:<8.3g}'.format(np.max(episode_returns)))
       logger.info('Min Episode Return:     {:<8.3g}'.format(np.min(episode_returns)))
 
