859b413b88cdcbff33c846bf9a2ff8df4bc42443
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-11-14 03:56:25 +0000

    Add setup script

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+  name='rl_replicas',
+  version=VERSION,
+  description='Reinforcement Learning Replications is a set of Pytorch implementations of reinforcement learning algorithms.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author='Yamato Kataoka',
+  url='https://github.com/yamatokataoka/reinforcement-learning-replications',
+  packages=find_packages(),
+  license='MIT',
+  keywords='reinforcement-learning',
+  install_requires=[
+    'gym[atari, box2d, classic_control]',
+    'numpy',
+    'scipy',
+    'torch>=1.4.0',
+    'tensorboard'
+  ],
+  python_requires='>=3.6'
+)
