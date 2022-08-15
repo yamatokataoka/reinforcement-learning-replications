127ad65df1dc82ef09b7364a895ccb1de111a9a7
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2022-08-15 14:24:59 +0000

    Refactor tag names

--- src/rl_replicas/algorithms/vpg.py
+++ src/rl_replicas/algorithms/vpg.py
@@ -96,22 +96,22 @@ class VPG:
             )
 
             self.metrics_manager.record_scalar(
-                "training/average_episode_return",
+                "sampling/average_episode_return",
                 float(np.mean(episode_returns)),
                 self.current_total_steps,
                 tensorboard=True,
             )
             self.metrics_manager.record_scalar(
-                "training/episode_return_std", float(np.std(episode_returns))
+                "sampling/episode_return_std", float(np.std(episode_returns))
             )
             self.metrics_manager.record_scalar(
-                "training/max_episode_return", float(np.max(episode_returns))
+                "sampling/max_episode_return", float(np.max(episode_returns))
             )
             self.metrics_manager.record_scalar(
-                "training/min_episode_return", float(np.min(episode_returns))
+                "sampling/min_episode_return", float(np.min(episode_returns))
             )
             self.metrics_manager.record_scalar(
-                "training/average_episode_length",
+                "sampling/average_episode_length",
                 float(np.mean(episode_lengths)),
                 self.current_total_steps,
                 tensorboard=True,
@@ -125,9 +125,7 @@ class VPG:
                 logger.debug("Save model")
                 self.save_model(current_epoch, model_path)
 
-            self.metrics_manager.record_scalar(
-                "training/time", time.time() - start_time
-            )
+            self.metrics_manager.record_scalar("time", time.time() - start_time)
 
             # Dump all metrics stored in this epoch
             self.metrics_manager.dump()
